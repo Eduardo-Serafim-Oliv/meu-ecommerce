f744604cf03ac48a45eb63c0a974d3ec8d3c5d2f
oluizfcs <luizfelipexilipe@gmail.com>
2025-12-31 00:16:55 -0300

    cadastro e exclusao de enderecos

---
+++ js/enderecos.js
@@ -0,0 +1,69 @@
+const container = document.getElementById("lista-enderecos");
+const form = document.querySelector("form");
+const toastCadastrado = bootstrap.Toast.getOrCreateInstance(document.getElementsByClassName("toast")[0]);
+const toastExcluido = bootstrap.Toast.getOrCreateInstance(document.getElementsByClassName("toast")[1]);
+const elementoModal = document.getElementById('cadastrarEndereco'); 
+const modal = bootstrap.Modal.getOrCreateInstance(elementoModal); 
+
+let enderecos = JSON.parse(localStorage.getItem("enderecos")) || [];
+
+carregarEnderecos();
+
+function adicionarCardEndereco(end) {
+    const card = document.createElement("div");
+    card.classList.add("card", "endereco", "mb-2");
+    card.innerHTML = `
+        <div class="card-body">
+            <div class="card-title"><strong>Endereço #${enderecos.indexOf(end) + 1}</strong></div>
+            <div class="card-text">Rua ${end.rua}, Nº ${end.numero}, ${end.cidade}/${end.estado}, CEP ${end.cep}</div>
+            <button class="btn btn-outline-secondary position-absolute" style="top: 7px; right: 7px;"><i class="bi bi-trash-fill"></i></button>
+        </div>
+    `;
+
+    container.appendChild(card);
+
+    card.querySelector("button").addEventListener("click", () => {
+        enderecos.splice(enderecos.indexOf(end), 1);
+        card.remove();
+        salvarEnderecos();
+        toastExcluido.show();
+        if(enderecos.length === 0) {
+            carregarEnderecos();
+        }
+    });
+}
+
+function salvarEnderecos() {
+    localStorage.setItem("enderecos", JSON.stringify(enderecos));
+}
+
+function carregarEnderecos() {
+    if (enderecos.length === 0) {
+        container.innerHTML = `<div class="fs-2 text-center mb-3">Não há endereços cadastrados.</div>`
+    } else {
+        container.innerHTML = ``;
+        enderecos.forEach(e => adicionarCardEndereco(e));
+    }
+}
+
+document.getElementById("btnCadastrar").addEventListener("click", () => {
+    form.classList.add('was-validated');
+
+    if (form.checkValidity()) {
+        const endereco = {
+            rua: document.getElementById("rua").value,
+            numero: document.getElementById("numero").value,
+            cidade: document.getElementById("cidade").value,
+            estado: document.getElementById("estado").value,
+            cep: document.getElementById("cep").value
+        };
+
+        enderecos.push(endereco);
+        adicionarCardEndereco(endereco);
+        salvarEnderecos();
+        form.reset();
+        form.classList.remove('was-validated');
+        modal.hide();
+        toastCadastrado.show();
+    }
+});
